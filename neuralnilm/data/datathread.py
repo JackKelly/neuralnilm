462a8b3d06610b5e07b4f9fe77311752a0ce9de7
Jack Kelly <jack-list@xlk.org.uk>
2015-08-20 13:16:08 +0100

    DataThread appears to work well :)

---
+++ neuralnilm/data/datathread.py
@@ -0,0 +1,31 @@
+from __future__ import print_function
+from threading import Thread, Event
+from Queue import Queue, Empty
+
+
+class DataThread(Thread):
+    def __init__(self, data_pipeline, max_queue_size=3, **get_batch_kwargs):
+        super(DataThread, self).__init__(name='neuralnilm-data-process')
+        self._stop = Event()
+        self._queue = Queue(maxsize=max_queue_size)
+        self.data_pipeline = data_pipeline
+        self._get_batch_kwargs = get_batch_kwargs
+
+    def run(self):
+        while not self._stop.is_set():
+            batch = self.data_pipeline.get_batch(**self._get_batch_kwargs)
+            self._queue.put(batch)
+
+    def get_batch(self, timeout=30):
+        if self.is_alive():
+            return self._queue.get(timeout=timeout)
+        else:
+            raise RuntimeError("Process is not running!")
+
+    def stop(self):
+        self._stop.set()
+        try:
+            self._queue.get(block=False)
+        except Empty:
+            pass
+        self.join()
