76df392da04efc3d642bbe87aa3ee1f7f56c74d5
Jack Kelly <jack-list@xlk.org.uk>
2015-08-28 07:48:33 +0100

    first attempt at putting validation costs into plot.  Forgot about folds!

--- neuralnilm/monitor/monitor.py
+++ neuralnilm/monitor/monitor.py
@@ -50,36 +50,65 @@ class Monitor(object):
         )
         return bool(document)
 
-    def _plot_train_scores(self):
+    def _get_mse(self, train_or_validation):
+        if train_or_validation == 'train':
+            collection = 'train_scores'
+            field = 'loss'
+        elif train_or_validation == 'validation':
+            collection = 'validation_scores'
+            field = 'scores.regression.mean_squared_error'
+        else:
+            raise ValueError(
+                "'{}' not valid argument.".format(train_or_validation))
+
+        # Get train scores
         monary = Monary()
         iterations, loss, source_id = monary.query(
             db='neuralnilm_experiments',
-            coll='train_scores',
+            coll=collection,
             query={'experiment_id': self.experiment_id},
-            fields=['iteration', 'loss', 'source_id'],
+            fields=['iteration', field, 'source_id'],
             types=['int32', 'float32', 'int8']
         )
 
-        df = pd.DataFrame(
+        scores_df = pd.DataFrame(
             {'loss': loss, 'source_id': source_id}, index=iterations)
 
+        return scores_df
+
+    def _plot_train_scores(self):
+        train_scores_df = self._get_mse('train')
+        validation_mse_df = self._get_mse('validation')
         fig, ax = plt.subplots(1)
-        sources = df['source_id'].unique()
+        sources = train_scores_df['source_id'].unique()
         for source_i in sources:
             # Get losses for just this source
-            mask = df['source_id'] == source_i
-            loss_for_source = df[mask]['loss']
+            def get_loss_for_source(df):
+                mask = df['source_id'] == source_i
+                return df[mask]['loss']
+            train_loss_for_source = get_loss_for_source(train_scores_df)
+            validation_mse_for_source = get_loss_for_source(validation_mse_df)
 
             # Downsample if necessary
-            loss_for_source = self._downsample(loss_for_source)
+            train_loss_for_source = self._downsample(train_loss_for_source)
+            validation_mse_for_source = self._downsample(
+                validation_mse_for_source)
 
             # Plot
-            ax.plot(loss_for_source.index, loss_for_source.values,
-                    label=str(source_i))
+            ax.plot(train_loss_for_source.index,
+                    train_loss_for_source.values,
+                    label='Train (source {:d})'.format(source_i))
+            if validation_mse_for_source.size > 0:
+                ax.plot(validation_mse_for_source.index,
+                        validation_mse_for_source.values,
+                        label='Validation (source {:d})'.format(source_i))
 
         ax.legend()
+        plt.title('Training costs')
+        ax.set_xlabel('Iteration')
+        ax.set_ylabel('Mean squared error')
         plt.show()
-        self._last_iteration_processed['train'] = iterations[-1]
+        self._last_iteration_processed['train'] = train_scores_df.index[-1]
 
     @property
     def validation_metric_names(self):
