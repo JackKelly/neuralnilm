47fa13567e85571a932cc14cf17b0caa1dcf137b
Jack Kelly <jack-list@xlk.org.uk>
2015-09-01 15:04:25 +0100

    added save_params and load_params.  Not yet tested.

--- neuralnilm/net.py
+++ neuralnilm/net.py
@@ -1,12 +1,20 @@
 from __future__ import print_function, division
 import theano
+import h5py
 from lasagne.layers.helper import get_all_layers, get_output
+from lasagne.layers import FeaturePoolLayer, DenseLayer, Conv1DLayer
 
 import logging
 logger = logging.getLogger(__name__)
 
 
 class Net(object):
+    """
+    Attributes
+    ----------
+    layers : list
+    train_iterations : int
+    """
     def __init__(self, output_layer):
         self.layers = get_all_layers(output_layer)
         self._deterministic_output_func = None
@@ -41,3 +49,62 @@ class Net(object):
         network_output = get_output(
             self.layers[-1], network_input, deterministic=deterministic)
         return network_output
+
+    def save_params(self, filename):
+        """
+        Save params to HDF in the following format:
+            /epoch<N>/L<I>_<type>/P<I>_<name>
+        """
+        mode = 'w' if self.train_iterations == 0 else 'a'
+        f = h5py.File(filename, mode=mode)
+        epoch_name = 'epoch{:06d}'.format(self.train_iterations)
+        try:
+            epoch_group = f.create_group(epoch_name)
+        except ValueError:
+            logger.exception("Cannot save params!")
+            f.close()
+            return
+
+        for layer_i, layer in enumerate(self.layers):
+            params = layer.get_params()
+            if not params:
+                continue
+            layer_name = 'L{:02d}_{}'.format(layer_i, layer.__class__.__name__)
+            layer_group = epoch_group.create_group(layer_name)
+            for param_i, param in enumerate(params):
+                param_name = 'P{:02d}'.format(param_i)
+                if param.name:
+                    param_name += "_" + param.name
+                data = param.get_value()
+                layer_group.create_dataset(
+                    param_name, data=data, compression="gzip")
+
+        f.close()
+
+    def load_params(self, filename, iteration):
+        """
+        Load params from HDF in the following format:
+            /epoch<N>/L<I>_<type>/P<I>_<name>
+        """
+        # Process function parameters
+        logger.info('Loading params from ' + filename + '...')
+
+        f = h5py.File(filename, mode='r')
+        epoch_name = 'epoch{:06d}'.format(iteration)
+        epoch_group = f[epoch_name]
+
+        for layer_i, layer in enumerate(self.layers):
+            params = layer.get_params()
+            if not params:
+                continue
+            layer_name = 'L{:02d}_{}'.format(layer_i, layer.__class__.__name__)
+            layer_group = epoch_group[layer_name]
+            for param_i, param in enumerate(params):
+                param_name = 'P{:02d}'.format(param_i)
+                if param.name:
+                    param_name += "_" + param.name
+                data = layer_group[param_name]
+                param.set_value(data.value)
+        f.close()
+        self.train_iterations = iteration
+        logger.info('Done loading params from ' + filename + '.')
