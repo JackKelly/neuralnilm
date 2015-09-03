b66324b254eceab4cd83019d373f65f2be3335e9
Jack Kelly <jack-list@xlk.org.uk>
2015-09-03 08:42:05 +0100

    only validated every 5000 updates

--- experiment_definitions/e575.py
+++ experiment_definitions/e575.py
@@ -68,7 +68,7 @@ def run(root_experiment_name):
                 metrics=Metrics(state_boundaries=[3]),
                 learning_rates={0: 1E-2},
                 repeat_callbacks=[
-                    (500, Trainer.validate),
+                    (5000, Trainer.validate),
                     (5000, Trainer.save_params),
                     (5000, Trainer.plot_estimates)
                 ]
